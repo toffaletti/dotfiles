2afe98597a6b53925aaafce4871b6c7fde47d920
Jason Toffaletti <toffaleti@gmail.com>
2013-02-25 13:14:16 -0800

    add install script

---
+++ install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+dir=$HOME/dotfiles
+olddir=$HOME/dotfiles_old
+files="vimrc zshrc"
+
+# create dotfiles_old in homedir
+echo "Creating $olddir for backup of any existing dotfiles in ~"
+mkdir -p $olddir
+echo "...done"
+
+# change to the dotfiles directory
+echo "Changing to the $dir directory"
+cd $dir
+echo "...done"
+
+echo "Moving any existing dotfiles from ~ to $olddir"
+for file in $files; do
+    mv -n ~/.$file $olddir/
+    echo "Creating symlink to $file in home directory."
+    ln -s $dir/$file ~/.$file
+done
